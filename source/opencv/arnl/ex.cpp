fe5d064985d810d592addac0f8b64b2278d5ddf3
quoioln <quoipro94@gmail.com>
2016-01-27 14:32:00 +0700

    update

--- source/opencv/arnl/ex.cpp
+++ source/opencv/arnl/ex.cpp
@@ -7,6 +7,17 @@ using namespace std;
 void addGoalDone(ArPose pose) {
 	ArLog::log(ArLog::Normal, "x = %f, y = %f", pose.getX(), pose.getY());
 }
+void addGoalFailed(ArPose pose){
+	ArLog::log(ArLog::Normal, "Fail at x = %f, y = %f", pose.getX(), pose.getY());
+}
+
+void addGoalFinished(ArPose pose) {
+	ArLog::log(ArLog::Normal, "Finished at x = %f, y = %f", pose.getX(), pose.getY());
+}
+
+void addGoalInterrupted(ArPose pose) {
+	ArLog::log(ArLog::Normal, "Interrupted at x = %f, y = %f", pose.getX(), pose.getY());
+}
 int main (int argc, char** argv) {
 	
 	Aria::init();
@@ -16,7 +27,7 @@ int main (int argc, char** argv) {
 	parser.loadDefaultArguments();
 	ArRobotConnector robotConnector(&parser, &robot);
 	//ArAnalogGyro gyro = new 
-	ArAnalogGyro gyro(&robot);
+//	ArAnalogGyro gyro(&robot);
 	if (!robotConnector.connectRobot()) {
 		ArLog::log(ArLog::Terse, "Could not connect to the robot.");
 		if(parser.checkHelpAndWarnUnparsed())
@@ -32,7 +43,7 @@ int main (int argc, char** argv) {
 	
 
 //	robot.runAsync(true);
-	
+	ArLog::log(ArLog::Normal, "OK");
 	ArMap map("office.map");
 	// set it up to ignore empty file names (otherwise if a configuration omits
 	// the map file, the whole configuration change will fail)
@@ -44,26 +55,38 @@ int main (int argc, char** argv) {
 
 
 	robot.runAsync(true);
-	robot.enableMotors();
+
 //	robot.lock();
-	robot.comInt(ArCommands::ENABLE, 1);
+//	robot.comInt(ArCommands::ENABLE, 1);
 	robot.addRangeDevice(&sonarDev);
-	ArSonarLocalizationTask locTask(&robot, &sonarDev, &map);
+//	ArSonarLocalizationTask locTask(&robot, &sonarDev, &map);
 //	ArActionAvoidFront avoidFront("avoid front obstacles");
 //	ArActionGoto gotoPoseAction("goto");
 //	robot.addAction(&gotoPoseAction, 50);
 //	robot.addAction(&avoidFront, 60);
 //	robot.moveTo(ArPose(0,0,0));
 	ArPathPlanningTask pathPlan(&robot, &sonarDev, &map);
+//	pathPlan.getRun
 //	locTask.localizeRobotAtHomeBlocking();
-	ArGlobalFunctor1<ArPose> add(&addGoalDone);// = new ArGlobalFunctor1(&addGoalDone);
-	pathPlan.addGoalDoneCB(&add);
+	ArGlobalFunctor1<ArPose> goalDone(&addGoalDone);// = new ArGlobalFunctor1(&addGoalDone);
+	ArGlobalFunctor1<ArPose> goalFail(&addGoalFailed);
+	ArGlobalFunctor1<ArPose> goalFinished(&addGoalFinished);
+	ArGlobalFunctor1<ArPose> goalInterrupted(&addGoalInterrupted);
+	pathPlan.addGoalDoneCB(&goalDone);
+	pathPlan.addGoalFailedCB(&goalFail);
+	pathPlan.addGoalFinishedCB(&goalFinished);
+	pathPlan.addGoalInterruptedCB(&goalInterrupted);
+
 //	ro
 //	ArActionPlanAndMoveToGoal gotoGoal (200, 10, pathPlan, NULL, &sonarDev);
 
 //	pathPlan.runAsync();
+	robot.enableMotors();
+//	while(!pathPlan.pathPlanToPose(ArPose(1000, 1000, 0), true, true));
 	pathPlan.pathPlanToPose(ArPose(1000, 1000, 0), true, true);
+//	pathPlan.pathPlanToPose(ArPose(1000, 1000, 0), true, true)
 	pathPlan.startPathPlanToLocalPose(true);
+//	pathPlan.getPathPlanActionGroup()->activate();
 //	pathPlan.getCurrentGoal()
 //	pathPlan.is
 //	robot.unlock();
@@ -78,7 +101,7 @@ int main (int argc, char** argv) {
 //	robot.lock();
 //	robot.setVel(200);
 //	robot.unlock();
-	while (!pathPlan.endPathPlanToLocalPose(true));
+	while (pathPlan.endPathPlanToLocalPose(true));
 		//ArUtil::sleep(1000);
 
 //	ArPose pose;
